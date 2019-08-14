f90b0dbafa8c633ebd1b3fa276a7f870c9584e86
Khullar <akhullar@internal.tavisca.com>
2019-08-14 11:48:13 +0530

    Hello hii api

---
+++ ApiTest/Controllers/ValuesController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTest.Controllers
+{
+    [Route("api/Test")]
+    public class ValuesController : ControllerBase
+    {
+
+        [HttpGet("Hii/{value}")]
+        public string GetHello(string value)
+        {
+            return $"Hello {value}";
+        }
+
+        [HttpGet("Hello/")]
+        public string GetHii()
+        {
+            return "Hii";
+        }
+        
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{value}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost("PostHii")]
+        public string Post([FromBody] string value)
+        {
+            return value;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
