b01d4733bec8690650dd153e8a57ce359487042f
Khullar <akhullar@internal.tavisca.com>
2019-08-14 19:14:24 +0530

    Done

--- ApiTest/Program.cs
+++ ApiTest/Program.cs
@@ -19,6 +19,6 @@ namespace ApiTest
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>().UseUrls(urls: "http://*:8999");
     }
 }
